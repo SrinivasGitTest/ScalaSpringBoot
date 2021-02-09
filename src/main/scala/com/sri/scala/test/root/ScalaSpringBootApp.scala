a621f8dfc530c21d92b29db7374f53c8c9a1a51e
Administrator <Administrator@192.168.0.12>
2021-02-09 15:07:38 -0500

    Fix-Swagger

--- src/main/scala/com/sri/scala/test/root/ScalaSpringBootApp.scala
+++ src/main/scala/com/sri/scala/test/root/ScalaSpringBootApp.scala
@@ -1,27 +1,38 @@
 package com.sri.scala.test.root
 
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.env.EnvironmentPostProcessor
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.ImportResource
 import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.core.env.StandardEnvironment
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.context.annotation.ImportResource
+import org.springframework.context.annotation.ComponentScan
 
 @EnableAutoConfiguration(exclude = {Array(classOf[DataSourceAutoConfiguration])})
 @ComponentScan(basePackages = Array("com.sri.scala.test.root"))
 @ImportResource(Array("classpath:spring/app-context.xml"))
 @SpringBootApplication
-class ScalaSpringBootApplication extends SpringBootServletInitializer with EnvironmentPostProcessor {
+class ScalaSpringBootApplication extends SpringBootServletInitializer with EnvironmentPostProcessor //with BeanDefinitionRegistryPostProcessor
+{
   
   @Override
 	  def postProcessEnvironment(environment : ConfigurableEnvironment , application : SpringApplication ) :Unit = {
 	    application.setAllowBeanDefinitionOverriding(true);
 	  }
   
+   // Members declared in org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
+  /*@Override def postProcessBeanDefinitionRegistry(beanReg : org.springframework.beans.factory.support.BeanDefinitionRegistry): Unit = {
+    
+  }
+  
+  // Members declared in org.springframework.beans.factory.config.BeanFactoryPostProcessor
+  @Override def postProcessBeanFactory(beanRegFact : org.springframework.beans.factory.config.ConfigurableListableBeanFactory): Unit = {
+    
+  }*/
 }
 
 /*object ScalaSpringBootApp 
