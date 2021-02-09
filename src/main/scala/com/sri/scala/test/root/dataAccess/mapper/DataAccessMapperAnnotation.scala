08122adef5da474e1f08e4fbf874d3dc188b9a6b
Administrator <Administrator@192.168.0.12>
2021-02-09 11:29:20 -0500

    Fix-With Annotation

--- src/main/scala/com/sri/scala/test/root/dataAccess/mapper/DataAccessMapperAnnotation.scala
+++ src/main/scala/com/sri/scala/test/root/dataAccess/mapper/DataAccessMapperAnnotation.scala
@@ -6,17 +6,16 @@ import org.apache.ibatis.annotations.Param
 import org.springframework.context.annotation.ComponentScan
 
 import com.sri.scala.test.root.model.UserReviewCase
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
 
-//@Mapper
+@Mapper
 @ComponentScan(basePackages = Array("com.sri.scala.test.root.dataAccess.mapper"))
 trait DataAccessMapperAnnotation {
   
-  /*@Select(Array("""<script>SELECT USERID userId, USER_ACTV_DATE userActDt, REVIEWR_NAME reviewerName, REVIEW_COUNT reviewCount, REVIEW_USEFUL reviewUseCount, 
+  @Select(Array("""<script>SELECT USERID userId, USER_ACTV_DATE userActDt, REVIEWR_NAME reviewerName, REVIEW_COUNT reviewCount, REVIEW_USEFUL reviewUseCount, 
                            REVIEW_FUNNY reviewFunnyCount, REF_USERID refUserId, REVIEW_FANS reviewFansCount, AVG_STAR_COUNT avgStartCount 
                            FROM user_review WHERE REVIEW_COUNT > #{reviewCt} <if test='reviewDate != null'> AND CAST(USER_ACTV_DATE AS DATE) >= #{reviewDate} </if></script>"""))
-  
-  def getuserReviewData(@Param("reviewCt") reviewCt : Integer, @Param("reviewDate")reviewDate : Date): Array[UserReviewCase];
-                           */
    def getuserReviewData(@Param("reviewCt") reviewCt : Integer, @Param("reviewDate")reviewDate : Date): Array[UserReviewCase];
   
   def test() {
